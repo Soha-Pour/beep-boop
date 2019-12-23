59fe21fea80b61ffbcb073fe7e4fd927cf58df48
Soha Pourpirali <pourpirali.soha@gmail.com>
2019-12-22 17:03:08 -0800

    respond with beep to 1, boop to 2, can't to 3

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,19 @@
-//Return range of numbers from 0 - userInput
 var finalOutput = function(userInput) {
-  var ansArr = [];
-  for (var index = 0; index <= userInput.length; index += 1) {
-    //Return "beep" for all digits containing a 1
-      if (userInput[index] === "1") {
+  for (var index = 0; index <= userInput; index += 1) {
+    var ansArr = [];
+    console.log(index);
+      if (index === 1) {
         console.log("Beep");
-
-    //Return "boop" for all digits containing a 2
-  } else if (userInput[index] === "2") {
+      } else if (index === 2) {
         console.log("Boop");
-
-    //Return "I'm sorry, I can't compute that" for all digits containing a 3
-  } else if (userInput[index] === "3") {
+      } else if (index ===3) {
         console.log("I'm sorry, I can't compute that");
       }
+    }
+
+  };
 
 
-
-  }
-};
-
-// //Return "beep" for all digits containing a 1
-//   if (userInput.includes("1")) {
-//     alert("Beep");
-//
-// //Return "boop" for all digits containing a 2
-//   } else if (userInput.includes("2")) {
-//     alert("Boop");
-//
-// //Return "I'm sorry, I can't compute that" for all digits containing a 3
-//   } else if (userInput.includes("3")) {
-//     alert("I'm sorry, I can't compute that");
-//   }
-// };
 
 
 $(document).ready(function() {
@@ -40,9 +21,8 @@ $(document).ready(function() {
   $("form#blanks").submit(function(event) {
     event.preventDefault();
 
-    var userInput = $("#user-input").val().split('');
+    var userInput = $("#user-input").val();
     var output = finalOutput(userInput);
-    console.log(userInput);
+    // console.log(userInput);
   });
-
 });
