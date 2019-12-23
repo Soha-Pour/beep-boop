4ff497cfab5c49e8936faa64e9760c318c4be66a
Soha Pourpirali <pourpirali.soha@gmail.com>
2019-12-22 21:48:57 -0800

    add functionality to deposit answer into a div on the page instead of an alert

--- js/scripts.js
+++ js/scripts.js
@@ -2,22 +2,24 @@ var ansArr = [""];
 var finalOutput = function(userInput) {
   for (var index = 0; index <= userInput; index++) {
     var indexArr = JSON.stringify(index).split('');
-    console.log(indexArr);
+
       if ((indexArr.includes("1")) && (indexArr.includes("2"))) {
         ansArr.push("Beep");
-        // console.log("Boop");
+
       } else if (indexArr.includes("3")) {
-        ansArr.push("I'm sorry, I can't compute that");
-        // console.log("I'm sorry, I can't compute that");
+        ansArr.push("CAN'T COMPUTE");
+
       } else if (indexArr.includes("1")) {
         ansArr.push("Beep");
-        // console.log("Beep");
+
       } else if ((indexArr.includes("2")) && (indexArr.includes("3"))) {
-        ansArr.push("I'm sorry, I can't compute that");
-        // console.log("I'm sorry, I can't compute that");
+        ansArr.push("CAN'T COMPUTE");
+
       } else if (indexArr.includes("2")) {
         ansArr.push("Boop");
-        // console.log("Boop");
+
+      } else {
+        ansArr.push(index);
       }
     }
   };
@@ -32,7 +34,8 @@ $(document).ready(function() {
 
     var userInput = $("#user-input").val();
     var output = finalOutput(userInput);
-    alert(ansArr);
+
+    $(".output").text(ansArr);
 
   });
 });
